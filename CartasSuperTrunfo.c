371b9f4748286f6fd1b86e248f438edd2d9b7f03
HelenCPro <cassia.helen96@gmail.com>
2025-08-12 21:26:13 -0300

    incluir dados matematicos no codigo fonte

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,16 @@ printf("Desafio Super Trunfo - Países\n");
 
 printf("Cadastro das Cartas\n");
 
-int população_1, população_2, população_3, população_4;                                      //Ex: População:12300000
-int pontos_turisticos_1, pontos_turisticos_2, pontos_turisticos_3, pontos_turisticos_4;     // Ex: Pontos turisticos: 50
-float área_1, área_2, área_3, área_4;                                                        // Ex: Area:1521.11 km²
-float pib_1, pib_2, pib_3, pib_4;                                                            //Ex: 699.28 bilhões de reais
-char estado_1[3], estado_2[3], estado_3[3], estado_4[3];                                     // Ex: "A"
-char cidade_1[50], cidade_2[50], cidade_3[50], cidade_4[50];                                   //Ex: "Belo Horizonte"
-char código_1[10], código_2[10], código_3[10], código_4[10];                                   // Ex: "A01"
+int população_1, população_2;                                                                //Ex: População:12300000
+int pontos_turisticos_1, pontos_turisticos_2;                                                // Ex: Pontos turisticos: 50
+float área_1, área_2;                                                                       // Ex: Area:1521.11 km²
+float pib_1, pib_2;                                                                         //Ex: 699.28 bilhões de reais
+char estado_1[3], estado_2[3];                                                               // Ex: "A"
+char cidade_1[50], cidade_2[50];                                                             //Ex: "Belo Horizonte"
+char código_1[10], código_2[10];                                                              // Ex: "A01"
+float divisão;
+float média;
+
 
 
 //Entrada de Dados
@@ -39,6 +42,11 @@ scanf("%f", &pib_1);
 printf("Númeos de Pontos Turísticos: "); 
 scanf("%d", &pontos_turisticos_1);
 
+printf(" Qual é a densidade populacional da carta 1?");
+scanf("%f", divisão);
+
+divisão = população_1 / área_1;
+
 
 //Carta 2
 
@@ -63,52 +71,7 @@ scanf("%f", &pib_2);
 printf("Númeos de Pontos Turísticos: "); 
 scanf("%d", &pontos_turisticos_2);
 
-//Carta 3
 
-printf("Estado: "); 
-scanf("%s", &estado_3);
-
-printf("Código: "); 
-scanf("%s", &código_3);
-
-printf("Nome da Cidade: "); 
-scanf("%s", &cidade_3);
-
-printf("População: "); 
-scanf("%d", &população_3);
-
-printf("Área: (km) "); 
-scanf("%f", &área_3);
-
-printf("PIB: (bilhões) "); 
-scanf("%f", &pib_3);
-
-printf("Númeos de Pontos Turísticos: "); 
-scanf("%d", &pontos_turisticos_3);
-
-//Carta 4
-
-
-printf("Estado: "); 
-scanf("%s", &estado_4);
-
-printf("Código: "); 
-scanf("%s", &código_4);
-
-printf("Nome da Cidade: "); 
-scanf("%s", &cidade_4);
-
-printf("População: "); 
-scanf("%d", &população_4);
-
-printf("Área: (km) "); 
-scanf("%f", &área_4);
-
-printf("PIB: (bilhões) "); 
-scanf("%f", &pib_4);
-
-printf("Númeos de Pontos Turísticos: "); 
-scanf("%d", &pontos_turisticos_4);
 
 
 
